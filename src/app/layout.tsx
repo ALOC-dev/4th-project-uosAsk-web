fb008ec0086cdeb82bec2f1765590afa23bdcc64
song9252 <song925252@gmail.com>
2025-10-04 23:56:47 +0900

    fix : Hydration Error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import ThemeProvider from '@/components/providers/theme-provider';
-import GlobalStyles from '@/components/global-styles';
 import './globals.css';
 
 const geistSans = Geist({
@@ -37,10 +35,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>
-          <GlobalStyles />
-          {children}
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
