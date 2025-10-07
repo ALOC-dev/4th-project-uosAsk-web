dbf50984e38b61400942c8c8683cf2501a9dd161
greengrape777 <junghoonkim0418@gmail.com>
2025-10-08 01:14:41 +0900

    refactor: 변경된 상수 데이터에 맞게 코드 수정

--- src/constants/department.ts
+++ src/constants/department.ts
@@ -1,5 +1,12 @@
-import departmentsData from './department.json' assert { type: 'json' };
+import universitiesData from './university.json' assert { type: 'json' };
 
-// import된 JSON은 런타임 값이므로 'as const' 불가 -> 타입 검사용 'satisfies' 사용
-export const DEPARTMENTS = departmentsData satisfies readonly string[];
-export type Deparment = typeof DEPARTMENTS[number];
+export interface University {
+    university: string;
+    departments: string[];
+}
+
+// UI용
+export const UNIVERSITIES: University[] = universitiesData;
+
+// API용
+export type Department = (typeof universitiesData[number]['departments'][number]);
