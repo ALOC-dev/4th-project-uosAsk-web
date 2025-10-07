cd875ff85e3bf07a07421ca1368edb262e21127c
greengrape777 <junghoonkim0418@gmail.com>
2025-10-08 00:45:52 +0900

    feat: 공지사항 API 설계

---
+++ src/services/notice/ListApi.ts
@@ -0,0 +1,25 @@
+import { apiClient } from "../api/client";
+import { Category, Deparment } from "@/constants";
+
+/*
+category 값을 넘길 때, Category 타입에서 index number로 접근해서 가져오기.
+department 값을 넘길 때, Department 타입에서 index number로 접근해서 Department[] 가져오기.
+*/
+interface GetNoticeListParams {
+    department: Deparment[],
+    keyword: string,
+    category: Category,
+    page: number,
+    exact: boolean,
+}
+
+export const getNoticeList = async (params: GetNoticeListParams) => {
+    const queryParams = {
+        ...params,
+        size: 15, // size 고정
+        department: params.department.join(','), // department에 overwrite하기
+    }
+    const response = await apiClient.get('/notice/search', { params: queryParams });
+
+    return response.data;
+}
