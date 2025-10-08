7d2935166b8fb4e6774d98bfaa4645cf5d6b9c65
tree105778 <bjyon0415@gmail.com>
2025-10-08 21:13:03 +0900

    feat : 새 채팅 기능 임시 구현

--- src/app/(chatBot)/page.tsx
+++ src/app/(chatBot)/page.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import { MainLayout } from '@/components/layout/main-layout';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
   return (
-    <MainLayout activeSection='chatbot'>
+    <MainLayout
+      activeSection='chatbot'
+      onNewChat={() => useRouter().replace('/')}
+    >
       <div>채팅봇 페이지 컨텐츠가 여기에 들어갑니다.</div>
     </MainLayout>
   );
