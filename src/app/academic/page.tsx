7fd01e34f3670c2612576e14e3d754849e564a9a
greengrape777 <junghoonkim0418@gmail.com>
2025-11-02 15:52:24 +0900

    feat: Academic 공지사항 요청 로직 추가

--- src/app/academic/page.tsx
+++ src/app/academic/page.tsx
@@ -2,12 +2,39 @@
 
 import { NoticeLayout } from '@/components/notice/notice-layout';
 import { AnimatedNoticeList } from '@/components/notice/notice-list';
+import { CATEGORIES } from '@/constants';
 import { academicNoticeData } from '@/data/notices';
+import { getNoticeList } from '@/services/notice/getNoticeList';
+import { NoticeApiResponse } from '@/types/notice';
+import { useEffect, useState } from 'react';
+
+const params = {
+  department: [],
+  keyword: '',
+  category: CATEGORIES[1],
+  page: 0,
+  exact: false,
+}
 
 export default function AcademicPage() {
+  const [notices, setNotices] = useState<NoticeApiResponse | undefined>();
+
+  useEffect(() => {
+    const fetchNotices = async () => {
+      try {
+        const response = await getNoticeList(params);
+        setNotices(response.data ?? response);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchNotices();
+  }, []);
+
   return (
     <NoticeLayout type='academic'>
-      <AnimatedNoticeList noticeData={academicNoticeData} />
+      <AnimatedNoticeList noticeData={notices} />
     </NoticeLayout>
   );
 }
