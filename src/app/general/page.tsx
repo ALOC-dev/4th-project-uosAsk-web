7207129ca118bc83b26470ffb19030e270a6a9c7
greengrape777 <junghoonkim0418@gmail.com>
2025-11-02 15:50:09 +0900

    리팩터링: Notice 타입을 Promise 방식으로 변경하고 loading 상태 제거

--- src/app/general/page.tsx
+++ src/app/general/page.tsx
@@ -5,6 +5,7 @@ import { AnimatedNoticeList } from '@/components/notice/notice-list';
 import { useEffect, useState } from 'react';
 import { getNoticeList } from '@/services/notice/getNoticeList';
 import { CATEGORIES, Category } from '@/constants';
+import { NoticeApiResponse } from '@/types/notice';
 
 const params = {
   department: [],
@@ -15,23 +16,20 @@ const params = {
 }
 
 export default function GeneralPage() {
-  const [notices, setNotices] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [notices, setNotices] = useState<NoticeApiResponse | undefined>();
 
-  useEffect( () => {
+  useEffect(() => {
     const fetchNotices = async () => {
       try {
-        const response = await getNoticeList(params)
+        const response = await getNoticeList(params);
         setNotices(response.data ?? response);
       } catch (e) {
         console.error(e);
-      } finally {
-        setLoading(false);
       }
     };
 
     fetchNotices();
-  }, [] )
+  }, []);
 
   return (
     <NoticeLayout type='general'>
