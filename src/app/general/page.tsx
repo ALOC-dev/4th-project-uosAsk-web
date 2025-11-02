18817d5e1f854a6bb83d4d32ee541e668a72c61d
greengrape777 <junghoonkim0418@gmail.com>
2025-11-02 15:38:37 +0900

    feat: 공지사항 요청 및 렌더링

--- src/app/general/page.tsx
+++ src/app/general/page.tsx
@@ -2,12 +2,40 @@
 
 import { NoticeLayout } from '@/components/notice/notice-layout';
 import { AnimatedNoticeList } from '@/components/notice/notice-list';
-import { generalNotices } from '@/data/notices';
+import { useEffect, useState } from 'react';
+import { getNoticeList } from '@/services/notice/getNoticeList';
+import { CATEGORIES, Category } from '@/constants';
+
+const params = {
+  department: [],
+  keyword: '',
+  category: CATEGORIES[0],
+  page: 0,
+  exact: false,
+}
 
 export default function GeneralPage() {
+  const [notices, setNotices] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect( () => {
+    const fetchNotices = async () => {
+      try {
+        const response = await getNoticeList(params)
+        setNotices(response.data ?? response);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNotices();
+  }, [] )
+
   return (
     <NoticeLayout type='general'>
-      <AnimatedNoticeList notices={generalNotices} />
+      <AnimatedNoticeList notices={notices} />
     </NoticeLayout>
   );
 }
